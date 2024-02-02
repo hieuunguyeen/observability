67f8c0d679c5013a766405bfb251e7090fce14c3
hieuunguyeen <blackevil245@gmail.com>
2024-02-02 17:58:37 +0200

    Add app to generate observable logs

---
+++ app/instrumentations.ts
@@ -0,0 +1,21 @@
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import {
+    PeriodicExportingMetricReader,
+    ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics'
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+
+const sdk = new NodeSDK({
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new ConsoleMetricExporter(),
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
+})
+
+sdk.start()
