f503b7f68df275f9ca4bca12b5c44ae800194391
hieuunguyeen <blackevil245@gmail.com>
2024-02-06 20:12:44 +0200

    Removes Fluentbit. All containers are green at least ...

--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,38 @@
 import express, { Express } from 'express';
+import { getLogger } from './instrumentations';
+import { SeverityNumber } from '@opentelemetry/api-logs';
 
 const PORT: number = parseInt(process.env.PORT || '8080');
 const app: Express = express();
+
+const APP = 'demo_app_log_instrumentation';
 
 function getRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 app.get('/roll', (req, res) => {
-    res.send(getRandomNumber(1, 100).toString());
+    const roll = getRandomNumber(1, 100).toString();
+    // getLogger().info(`Sending back: ${roll}`);
+    getLogger(APP).emit({
+        severityNumber: SeverityNumber.INFO,
+        timestamp: Date.now(),
+        body: `Sending back: ${roll}`,
+        attributes: {
+            roll,
+        },
+    });
+    res.send(roll);
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening for requests on http://localhost:${PORT}`);
+    // getLogger().info(`Listening for requests on http://localhost:${PORT}`);
+    getLogger(APP).emit({
+        severityNumber: SeverityNumber.INFO,
+        timestamp: Date.now(),
+        body: `Listening for requests on http://localhost:${PORT}`,
+        attributes: {
+            listening: 'port',
+        },
+    });
 });
--- app/instrumentations.ts
+++ app/instrumentations.ts
@@ -1,21 +1,77 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
-    PeriodicExportingMetricReader,
-    ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+    NodeSDK,
+    logs,
+    metrics,
+    resources,
+    api,
+} from '@opentelemetry/sdk-node';
 
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 
-const sdk = new NodeSDK({
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
-    instrumentations: [getNodeAutoInstrumentations()],
-});
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+// import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston';
 
-sdk.start();
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+// import winston from 'winston';
+
+// Setup trace and metric exporters via sdk. Logs needs to be done outside of SDK
+// because it is not yet supported
+function startOTLPInstrumentation() {
+    const sdk = new NodeSDK({
+        traceExporter: new OTLPTraceExporter({
+            url: 'http://localhost:4318/v1/traces',
+        }),
+        metricReader: new PrometheusExporter({
+            host: '0.0.0.0',
+            port: 9464,
+            endpoint: '/metrics',
+            appendTimestamp: true,
+        }),
+        logRecordProcessor: new logs.SimpleLogRecordProcessor(
+            new OTLPLogExporter({
+                url: 'http://localhost:4318/v1/logs',
+            }),
+        ),
+        // metricReader: new metrics.PeriodicExportingMetricReader({
+        //     exporter: new OTLPMetricExporter({
+        //         url: 'http://localhost:4318/v1/metrics',
+        //     }),
+        // }),
+        instrumentations: [
+            new ExpressInstrumentation(),
+            // new WinstonInstrumentation(),
+        ],
+    });
+
+    sdk.start();
+
+    process.on('SIGTERM', () => {
+        sdk.shutdown().then(
+            () => console.log('otel sdk shut down successfully'),
+            (err) => console.log('Error shutting down otel sdk', err),
+        );
+    });
+}
+
+export function getLogger(name: string) {
+    const logger = new logs.LoggerProvider({
+        resource: new resources.Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: 'james',
+        }),
+    });
+
+    process.on('SIGTERM', () => {
+        logger.forceFlush().then(
+            () => console.log('otel log provider flushes successfully'),
+            (err) => console.log('Error flushing otel log provider', err),
+        );
+    });
+
+    return logger.getLogger(name);
+}
+
+startOTLPInstrumentation();
